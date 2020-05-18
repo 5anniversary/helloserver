2d763ffff38c3f933ca8d21142b4d491ab473887
Junhyeon <oh.junhyeon95@gmail.com>
2020-05-19 03:58:54 +0900

    for deploy

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -24,6 +24,7 @@ public func configure(_ config: inout Config,
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
     // middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
+    middlewares.use(FileMiddleware.self)
     services.register(middlewares)
     
     var databases = DatabasesConfig()
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,9 +5,24 @@ import Vapor
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.get { req in
-        return "It works!"
+        return try req.view().render("index")
     }
-    
+    router.get("about") { req -> Future<View> in
+      return try req.view().render("about")
+    }
+    router.get("activity") { req -> Future<View> in
+      return try req.view().render("activity")
+    }
+    router.get("project") { req -> Future<View> in
+      return try req.view().render("project")
+    }
+    router.get("recruit") { req -> Future<View> in
+      return try req.view().render("recruit")
+    }
+    router.get("contact") { req -> Future<View> in
+      return try req.view().render("contact")
+    }
+
     // Basic "Hello, world!" example
     router.get("hello") { req in
         return "Hello, world!"
@@ -17,8 +32,8 @@ public func routes(_ router: Router) throws {
         return User.query(on: req).all()
     }
     
-    router.get("view") { req -> Future<View> in
-      return try req.view().render("index")
+    router.get("example") { req -> Future<View> in
+      return try req.view().render("example")
     }
 
     router.get("bonus") { req -> Future<View> in
